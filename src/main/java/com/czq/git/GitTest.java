e0ca3cff06aab95c70ade11447b3aea8394aa3ba
ceeziin <ceeziin@163.com>
2022-04-18 19:31:08 +0800

    test IDEA git22

--- src/main/java/com/czq/git/GitTest.java
+++ src/main/java/com/czq/git/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("1 + 1 = 1");
         System.out.println("1 + 1 = 2");
-        System.out.println("1 + 1 = 2");
+        System.out.println("1 + 1 = 3");
         System.out.println("1 + 1 = 2");
         System.out.println("1 + 1 = 2");
     }
