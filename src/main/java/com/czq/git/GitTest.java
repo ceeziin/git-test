e4eca04d6a3e7e582cca97b427f378a717bec0ac
ceeziin <ceeziin@163.com>
2022-04-18 21:24:44 +0800

    commitone

--- src/main/java/com/czq/git/GitTest.java
+++ src/main/java/com/czq/git/GitTest.java
@@ -10,12 +10,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("push test2");
         System.out.println("push test2");
-        System.out.println("push test2");
-        System.out.println("push test2");
-        System.out.println("push test2");
-        System.out.println("push test2");
-        System.out.println("push test2");
-        System.out.println("push test2");
 
     }
 }
