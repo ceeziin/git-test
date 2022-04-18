fab08972a5f91f958c5f2dc2ece090e74fe5a0b0
ceeziin <ceeziin@163.com>
2022-04-18 19:33:07 +0800

    test IDEA git22

--- src/main/java/com/czq/git/GitTest.java
+++ src/main/java/com/czq/git/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
         System.out.println("1 + 1 = 1");
         System.out.println("1 + 1 = 2");
         System.out.println("1 + 1 = 3");
-        System.out.println("1 + 1 = 2");
+        System.out.println("1 + 1 = 4");
         System.out.println("1 + 1 = 2");
     }
 }
