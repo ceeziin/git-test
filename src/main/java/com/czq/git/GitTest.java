816538f3088e1e54e404bd86596ecde4a22d5a42
ceeziin <ceeziin@163.com>
2022-04-18 19:41:38 +0800

    test IDEA git77

--- src/main/java/com/czq/git/GitTest.java
+++ src/main/java/com/czq/git/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
         System.out.println("1 + 1 = 2");
         System.out.println("1 + 1 = 3");
         System.out.println("1 + 1 = 4");
-        System.out.println("1 + 1 = 5");
+        System.out.println("1 + 1 =77");
     }
 }
